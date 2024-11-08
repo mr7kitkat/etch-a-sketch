d819b039640582e4de399ae2d2a0cb8fe99d8c09
mr7kitkat <mr7kitkat@gmail.com>
2024-11-08 15:35:01 +0530

    make desktop and mobile layout with menu

---
+++ scripts/main.js
@@ -0,0 +1,19 @@
+const menu = document.querySelector(".menu");
+const button = document.querySelector(".btn");
+
+button.addEventListener("click", function () {
+  if (this.dataset.status == 0) {
+    menu.classList.add("animate");
+    this.dataset.status = 1;
+    this.innerText = "CLOSE";
+  } else if (this.dataset.status == 1) {
+    this.dataset.status = 0;
+    this.innerText = "MENU";
+
+    menu.classList.add("remove");
+    setTimeout(() => {
+      menu.classList.remove("animate");
+      menu.classList.remove("remove");
+    }, 500);
+  }
+});
